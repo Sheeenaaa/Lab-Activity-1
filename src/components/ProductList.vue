8d4672990a5480c12ab031a2f91f115ece6b61f2
Sheeenaaa <167487943+Sheeenaaa@users.noreply.github.com>
2025-03-06 18:26:47 +0800

    Add files via upload

---
+++ src/components/ProductList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Products</h2>
+    <div v-for="product in products" :key="product.id">
+      <p>{{ product.name }} - ₱{{ product.price }}</p>
+      <button @click="addToCart(product)">Add to Cart</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState(['products'])
+  },
+  methods: {
+    ...mapActions(['addToCart'])
+  }
+};
+</script>
---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Shopping Cart</h2>
+    <ul>
+      <li v-for="item in cartItems" :key="item.id">
+        {{ item.name }} - ₱{{ item.price }} x {{ item.quantity }}
+        <button @click="decreaseQuantity(item.id)">-</button>
+        <button @click="addToCart(item)">+</button>
+      </li>
+    </ul>
+    <p>Total Amount: ₱{{ $store.getters.cartTotal }}</p>
+    <p>Total Items: {{ $store.getters.cartQuantity }}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters(['cartItems', 'cartTotal'])
+  },
+  methods: {
+    ...mapActions(['addToCart', 'decreaseQuantity'])
+  }
+};
+</script>
